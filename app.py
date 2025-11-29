817c5a6eb9933d8062a2fdc6a3a95c5bc58797fb
vikkya <vikkya94@yahoo.com>
2025-11-29 13:42:04 +0530

    added route

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return {"status": "API is working!"}
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
